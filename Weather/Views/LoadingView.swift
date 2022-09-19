db62c713eface5e71bc3daa9a8c77cfc31a37e82
Dilmurod <karimovd892@gmail.com>
2022-09-19 21:31:28 +0500

    initial commit

---
+++ Weather/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  Weather
+//
+//  Created by Karimov Dilmurod on 19/09/22.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
